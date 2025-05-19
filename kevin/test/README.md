54daf623cc630f302be5e951d58417ec570c775a
kevin sony <kevinsony2003@gmail.com>
2025-05-18 21:20:17 -0500

    Simplified Design significantly

--- kevin/test/README.md
+++ kevin/test/README.md
@@ -5,7 +5,7 @@ The goal is to facilitate communication between two computers on a local network
 
 ## SimpleChat
 
-SimpleChat achieves this goal by making the peer both a client and a server. The assumption made is that one peer is aware of the address of the other peer. In this way, communication occurs afterwards.
+SimpleChat achieves this goal by making the peer both a client and a server. The assumption made is that one peer is aware of the address of the other peer. Each peer runs two threads, a child thread with a ServerSocket to listen for connections, and a parent thread waiting for a connection to start writing. When the ServerSocket finally accepts, the child firsts sends information to the parent thread containing the information needed to connect to the other peer's ServerSocket. The child then continues reading information from the peer. Likewise, the parent thread writes information to the peer. In this way, communication occurs.
 
 ## BroadcastChat
 
--- kevin/test/SimpleChat.java
+++ kevin/test/SimpleChat.java
@@ -1,63 +1,61 @@
 package kevin.test;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.InterfaceAddress;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
+import java.net.*;
+import java.io.*;
 
 /*
  * Create a peer-to-peer chat. In this SimpleChat model, we assume one peer knows the (local) address of another
- * peer. The peer that knows should establish a connection. Use semaphores for preventing simultaneous read and write
- * by same process. We assume only one communication channel between different chats.
- * 
+ * peer. The peer that knows should establish a connection. We assume only one communication channel between different chats.
  */
 
-public class SimpleChat implements Runnable {
-
-	private int mode;
-
-	/*
-	 * If mode is 0 (meaning no arguments passed), then this instance will not be making the connection.
-	 * Otherwise, it will be making the connection.
-	 *
-	 */	
-	public SimpleChat(int mode) {
-		this.mode = mode;
-	}
+public class SimpleChat {
 	
-
-	public static void main(String[] args) throws Exception {
-		SimpleChat chat = new SimpleChat(args.length);
-		
-		if (args.length == 0) {
-			chat.run();
-		} else {
-			semaphore 
-		}
+	public static void main(String[] args) throws Exception { 
+		Socket peer;
+		BufferedReader inputFromPeerSocket;
+		PrintWriter outputToSocket;
+		BufferedReader inputFromTerminal;
 
 		/*
-		while(true) {
-			String broadc
-			Thread.sleep(1);
-		}
-		*/
-
-	}
-
-	public void run() {
-		/*
-		 * This listens for the connection
-		 *
+		 * In the case where there is an argument is supplied, there should be only two arguments supplied: the address of the other chat and the port to connect to
+		 * If no arguments are supplied, listen for incoming requests. For now, hard-code the port to listen to at 5001
+		 * 
 		 */
-		System.out.println("Main Child Thread");
-		
+		if (args.length == 0) {
+			ServerSocket serverSocket = new ServerSocket(5001);
+			peer = serverSocket.accept();
+			serverSocket.close();
+			
+			inputFromPeerSocket = new BufferedReader(new InputStreamReader(peer.getInputStream()));
+			outputToSocket = new PrintWriter(peer.getOutputStream(), true);
+			inputFromTerminal = new BufferedReader(new InputStreamReader(System.in));
+			
+		} else {
+			peer = new Socket(args[0], Integer.parseInt(args[1]));
 
-		Socket s = new Socket();
+			inputFromPeerSocket = new BufferedReader(new InputStreamReader(peer.getInputStream()));
+			outputToSocket = new PrintWriter(peer.getOutputStream(), true);
+			inputFromTerminal = new BufferedReader(new InputStreamReader(System.in));
+
+			outputToSocket.println("Connecting...");
+		}
+
+
+		String socketInput = "";
+		String userInput = "";
+		
+		do {
+			// read from the peer
+			socketInput = inputFromPeerSocket.readLine();
+			System.out.printf("(%s) > %s\n", peer.getInetAddress().toString(), socketInput);
+
+			// read from the terminal and output to peer socket
+			userInput = inputFromTerminal.readLine();
+			outputToSocket.println(userInput);
+
+		} while (!userInput.toLowerCase().equals("bye") && !socketInput.toLowerCase().equals("bye")); 
+
+
+		peer.close();
 
 	}
 }
